8901bd103aa569c49cde02f8c678b6229a0aaf6e
Jenya Vasilyev <Vasilyev@aplsud.ki.court.local>
2019-01-22 18:25:24 +0200

    start add signup.php

--- setup.php
+++ setup.php
@@ -1,48 +1,39 @@
 <!DOCTYPE html>
-<html lang="ru">
+<html>
 <head>
-    <meta charset="utf-8">
-    <title>Databases Config</title>
+    <title>Setting up database</title>
 </head>
 <body>
 
-<h3>Settings up</h3>
+<h3>Setting up...</h3>
 
-<?php
-require_once 'function.php';
-
-createTable(
-    'members',
-    'user VARCHAR(16), 
-            pass VARCHAR(16),
-            INDEX(user(6))');
-
-createTable(
-    'messages',
+<?php // Example 26-3: setup.php
+require_once 'functions.php';
+createTable('members',
+    'user VARCHAR(16),
+              pass VARCHAR(16),
+              INDEX(user(6))');
+createTable('messages',
     'id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            auth VARCHAR(16),
-            recip VARCHAR(16),
-            pm CHAR(1),
-            time INT UNSIGNED,
-            message VARCHAR(4096),
-            INDEX (auth(6)),
-            INDEX (recip(6))');
-
-createTable(
-    'friend',
+              auth VARCHAR(16),
+              recip VARCHAR(16),
+              pm CHAR(1),
+              time INT UNSIGNED,
+              message VARCHAR(4096),
+              INDEX(auth(6)),
+              INDEX(recip(6))');
+createTable('friends',
     'user VARCHAR(16),
-            friend VARCHAR(16),
-            INDEX (user(6)),
-            INDEX (friend(6))');
-
-createTable(
-    'profiles',
+              friend VARCHAR(16),
+              INDEX(user(6)),
+              INDEX(friend(6))');
+createTable('profiles',
     'user VARCHAR(16),
-           text VARCHAR(4096),
-           INDEX(user(6))');
+              text VARCHAR(4096),
+              INDEX(user(6))');
 ?>
 
-<br>
+<br>...done.
 </body>
 </html>
 
---
+++ signup.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vasilyev
+ * Date: 22.01.2019
+ * Time: 17:34
+ */
+
+require_once 'functions.php';
+echo <<<_END
+<script>
+function checkUser(user)
+{
+    if (user.value == '')
+        {
+            
+        }
+}
+</script>
+_END;
